93b31ca4345385a51fa7630c403902d4f885ce88
maayan <mayanpotu500@gmail.com>
2025-07-31 08:06:47 +0300

    new commit

--- src/abc_digit_recognition.cpp
+++ src/abc_digit_recognition.cpp
@@ -1,6 +1,6 @@
 #include "../include/painter.hpp"
-#include "Globals.hpp"
 #include "../include/transformation.hpp"
+#include "Globals.hpp"
 #include <iostream>
 #include <model.hpp>
 
@@ -42,14 +42,12 @@ static nn::global::Transformation finalEvaluate = [](const nn::global::ParamMetr
 	return display.getValues();
 };
 
-const int EMNIST_BALANCED_MAP[62] = {
-    48, 49, 50, 51, 52, 53, 54, 55, 56, 57,     // 0–9 → '0'–'9'
-    65, 66, 67, 68, 69, 70, 71, 72, 73, 74,     // 10–19 → 'A'–'J'
-    75, 76, 77, 78, 79, 80, 81, 82, 83, 84,     // 20–29 → 'K'–'T'
-    85, 86, 87, 88, 89, 90,                     // 30–35 → 'U'–'Z'
-    97, 98, 99, 100, 101, 102, 103, 104, 105,   // 36–44 → 'a'–'i'
-    106, 107, 108, 109, 110, 111, 112, 113,     // 45–52 → 'j'–'q'
-    114, 115, 116, 117, 118, 119, 120, 121, 122 // 53–61 → 'r'–'z'
+const int EMNIST_BALANCED_MAP[47] = {
+    48, 49, 50, 51, 52, 53, 54, 55, 56, 57,             // 0–9 → '0'-'9'
+    65, 66, 67, 68, 69, 70, 71, 72, 73, 74,             // 10–19 → 'A'-'J'
+    75, 76, 77, 78, 79, 80, 81, 82, 83, 84,             // 20–29 → 'K'-'T'
+    85, 86, 87, 88, 89, 90,                             // 30–35 → 'U'-'Z'
+    97, 98, 100, 101, 102, 103, 104, 110, 113, 116, 117 // 36–46 → 'a','b','d','e','f','g','h','n','q','t','u'
 };
 
 int main(int argc, char *argv[]) {
@@ -63,12 +61,12 @@ int main(int argc, char *argv[]) {
 			model.load("emnist_model.txt");
 		} else if (arg == "-t") {
 			std::cout << "training command emnist\n";
-			model.train("../ModelData/emnist_train_data", doTransform);
+			model.train("../ModelData/emnist_balanced_train_data", doTransform);
 			model.save("emnist_model.txt");
 		}
 	}
 
-	nn::model::modelResult result = model.evaluateModel("../ModelData/emnist_test_data", finalEvaluate);
+	nn::model::modelResult result = model.evaluateModel("../ModelData/emnist_balanced_test_data", finalEvaluate);
 	printf("prediction: %f\n", result.percentage);
 
 	App display;
@@ -79,7 +77,7 @@ int main(int argc, char *argv[]) {
 		model.runModel(display.getValues());
 		nn::global::Prediction pre = model.getPrediction();
 
-		char character = (pre.index < 62) ? static_cast<char>(EMNIST_BALANCED_MAP[pre.index]) : '?';
+		char character = (pre.index < 47) ? static_cast<char>(EMNIST_BALANCED_MAP[pre.index]) : '?';
 
 		printf("output: %zu, %f, char: %c\n", pre.index, pre.value, character);
 	}
